0511981ac1e784854dfb5d87dfc358a1e3a28684
JesusRosalesDevapps <jesus.rosales@devapps.com.mx>
2026-02-03 16:50:10 -0600

    creacion de entity

--- PrimerPractica/backend/crud-fullstack-angular/src/main/java/com/example/crudfullstack/crud_fullstack_angular/CrudFullstackAngularApplication.java
+++ PrimerPractica/backend/crud-fullstack-angular/src/main/java/com/example/crudfullstack/crud_fullstack_angular/CrudFullstackAngularApplication.java
@@ -8,6 +8,9 @@ public class CrudFullstackAngularApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudFullstackAngularApplication.class, args);
+
+
+		
 	}
 
 }
---
+++ PrimerPractica/backend/crud-fullstack-angular/src/main/java/com/example/crudfullstack/crud_fullstack_angular/entity/customer.java
@@ -0,0 +1,58 @@
+package com.example.crudfullstack.crud_fullstack_angular.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+    public customer () {
+    
+    }
+
+    public customer(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
